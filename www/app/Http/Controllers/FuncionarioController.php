c1449a678d1224b876b6789e7986a5acc0b44a9a
cavebran <guilherme.cavequia@gmail.com>
2021-06-21 22:56:47 -0300

    Login totalmente funcional

--- www/app/Http/Controllers/FuncionarioController.php
+++ www/app/Http/Controllers/FuncionarioController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ModelFuncionario;
 use Illuminate\Http\Request;
 
 class FuncionarioController extends Controller
 {
     public function FuncionarioLogin(Request $request) {
         $data = $request->input();
-        $request->session()->put('user', $data['user']);
-        return redirect('/sessoes');
+        $funcionario = ModelFuncionario::find($data['user']);
+        
+        if($funcionario) {
+            $funcionario = $funcionario->toArray();
+        }
+
+        if($funcionario && $funcionario['senha'] == $data['pass'] && $funcionario['cargo'] == 'GERENTE') {
+            $request->session()->put('user', $funcionario['nome']);
+            return redirect('/sessoes');
+        }
+        else {
+            return redirect('/');
+        }
     }
 }
--- www/resources/views/templates/template.blade.php
+++ www/resources/views/templates/template.blade.php
@@ -1,6 +1,5 @@
 @php
 if(!session('user')) {
-    echo 'ixi';
     header("Location: /");
 	exit();
 }
